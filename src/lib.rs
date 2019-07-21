ff460a485c4f59395ac28e05917dfba121a2be53
Dmitriy Belyaev <mail@d-belyaev.ru>
2019-07-21 09:05:23 +0300

    borrow input, not owned

--- src/lib.rs
+++ src/lib.rs
@@ -15,25 +15,25 @@ mod tests {
 
     #[test]
     fn integers() {
-        let result = merge_sort(vec![9,8,7,6,5,4,3,2,1], |a, b| a.cmp(&b));
+        let result = merge_sort(&vec![9,8,7,6,5,4,3,2,1], |a, b| a.cmp(&b));
         assert_eq!(vec![1,2,3,4,5,6,7,8,9], result);
-        let result = merge_sort(vec![8,7,6,5,4,3,2,1], |a, b| a.cmp(&b));
+        let result = merge_sort(&vec![8,7,6,5,4,3,2,1], |a, b| a.cmp(&b));
         assert_eq!(vec![1,2,3,4,5,6,7,8], result);
-        let result = merge_sort_cmp(vec![8,7,6,5,4,3,2,1]);
+        let result = merge_sort_cmp(&vec![8,7,6,5,4,3,2,1]);
         assert_eq!(vec![1,2,3,4,5,6,7,8], result);
     }
 
     #[test]
     fn floats() {
-        let result = merge_sort(vec![9.0,8.0,7.0,6.0,5.0,4.0,3.0,2.0,1.0], |a, b| compare_f64(&a, &b));
+        let result = merge_sort(&vec![9.0,8.0,7.0,6.0,5.0,4.0,3.0,2.0,1.0], |a, b| compare_f64(&a, &b));
         assert_eq!(vec![1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0], result);
-        let result = merge_sort(vec![8.0,7.0,6.0,5.0,4.0,3.0,2.0,1.0], |a, b| compare_f64(&a, &b));
+        let result = merge_sort(&vec![8.0,7.0,6.0,5.0,4.0,3.0,2.0,1.0], |a, b| compare_f64(&a, &b));
         assert_eq!(vec![1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0], result);
     }
 
     #[test]
     fn single_element() {
-        assert_eq!(vec![0], merge_sort(vec![0], |_, _| Ordering::Equal))
+        assert_eq!(vec![0], merge_sort(&vec![0], |_, _| Ordering::Equal))
     }
 
     #[test]
@@ -44,7 +44,7 @@ mod tests {
         }
         assert_eq!(
             vec![TestStruct{f: -0.5}, TestStruct{f: 0.5}],
-            merge_sort(vec![TestStruct{f: 0.5}, TestStruct{f: -0.5}], |a, b| compare_f64(&a.f, &b.f))
+            merge_sort(&vec![TestStruct{f: 0.5}, TestStruct{f: -0.5}], |a, b| compare_f64(&a.f, &b.f))
         );
     }
 }
@@ -52,7 +52,7 @@ mod tests {
 use std::rc::Rc;
 use std::cmp::{Ordering, Ord};
 
-pub fn merge_sort<T, F>(input: Vec<T>, compare: F) -> Vec<T>
+pub fn merge_sort<T, F>(input: &Vec<T>, compare: F) -> Vec<T>
 where
     T: Clone,
     F: Fn(T, T) -> Ordering
@@ -61,30 +61,30 @@ where
     merge_sort_internal(input, compare)
 }
 
-pub fn merge_sort_cmp<T: Clone + Ord>(input: Vec<T>) -> Vec<T> {
+pub fn merge_sort_cmp<T: Clone + Ord>(input: &Vec<T>) -> Vec<T> {
     let compare = Rc::new(Box::new(|a: T, b: T| a.cmp(&b)));
     merge_sort_internal(input, compare)
 }
 
-fn merge_sort_internal<T, F>(input: Vec<T>, compare: Rc<Box<F>>) -> Vec<T>
+fn merge_sort_internal<T, F>(input: &Vec<T>, compare: Rc<Box<F>>) -> Vec<T>
 where
     T: Clone,
     F: Fn(T, T) -> Ordering
 {
     let len = input.len();
     if len == 1 {
-        return input;
+        return vec![input[0].clone()];
     }
     let half_len = len / 2;
     let left = {
         let mut left = Vec::with_capacity(half_len);
         left.extend_from_slice(&input[..half_len]);
-        merge_sort_internal(left, compare.clone())
+        merge_sort_internal(&left, compare.clone())
     };
     let right = {
         let mut right = Vec::with_capacity(len - half_len);
         right.extend_from_slice(&input[half_len..]);
-        merge_sort_internal(right, compare.clone())
+        merge_sort_internal(&right, compare.clone())
     };
     let mut result = Vec::with_capacity(len);
     let mut i = 0;
